d280d663d3a5e937e48bd4e60e87b75282f412a3
Iago Machado <iago.machado000@gmail.com>
2025-02-10 22:42:44 -0300

    04-04-carregamento-automatico

--- 04-php-orientado-a-objetos/04-04-carregamento-automatico/index.php
+++ 04-php-orientado-a-objetos/04-04-carregamento-automatico/index.php
@@ -7,8 +7,33 @@ fullStackPHPClassName("04.04 - Carregamento automático");
  */
 fullStackPHPClassSession("autoload spl psr-4", __LINE__);
 
+// require __DIR__ . '/source/Loading/Address.php';
+// require __DIR__ . '/source/Loading/Company.php';
+// require __DIR__ . '/source/Loading/User.php';
+
+require __DIR__ . '/source/autoload.php';
+
+$address = new \Source\Loading\Address();
+$company = new \Source\Loading\Company();
+$user = new \Source\Loading\User();
+
+echo '<pre>';
+var_dump(
+    $address,
+    $company,
+    $user
+);
+echo '</pre>';
 
 /*
  * [ autoload composer psr-4 ] https://getcomposer.org/doc/00-intro.md
  */
 fullStackPHPClassSession("autoload composer psr-4", __LINE__);
+
+require __DIR__ . "/vendor/autoload.php";
+
+$mail = new \PHPMailer\PHPMailer\PHPMailer(); 
+
+echo '<pre>';
+var_dump($mail);
+echo '</pre>';
---
+++ 04-php-orientado-a-objetos/04-04-carregamento-automatico/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'iagomachado/04-04-carregamento-automatico',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1692dc5ff5dddff0e9aaa217d61450de453e073c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'iagomachado/04-04-carregamento-automatico' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1692dc5ff5dddff0e9aaa217d61450de453e073c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.3',
+            'version' => '6.9.3.0',
+            'reference' => '2f5c94fe7493efc213f643c23b1b1c249d40f47e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
